563e2c33dd02f74fd0b97c6f39049d9f2c9d13b8
OficialGio <55326287+OficialGio@users.noreply.github.com>
2019-09-28 11:12:38 -0500

    Initial commit

---
+++ practiceVariables.py
@@ -0,0 +1,65 @@
+a = 4
+a = 3.45
+a = "string"
+a = True
+a = [4, 3.45, "string", True]
+print(a[2])
+
+name = "Gio"
+print(name)
+
+
+
+
+var1 = 5
+print(var1)
+var2 = 10
+print(var2)
+var3 = var2
+print(var3)
+x = 24
+print(x)
+y = 100
+print(y)
+
+z = 1.1
+print(z)
+a = 2.75
+print(a)
+b = a
+print(b)
+c = 100.1
+print(c)
+d = 12.3
+print(d)
+
+name = "Gio"
+print(name)
+e = "G"
+print(e)
+f = "o"
+print(f)
+g = "chicken"
+print(g)
+h = name[0] + name[1] + name[2]
+print(h)
+
+done = "False"
+print(done)
+smart = "True"
+print(smart)
+i = "done"
+print(i)
+j = "False"
+print(j)
+almostDone = "True"
+print(almostDone)
+
+
+listInt = [var1, var2, var3, x, y]
+print(listInt[2])
+listString = [z, a, b, c, d]
+print(listString[3])
+listBoolean = [done, smart, i, j, almostDone]
+print(listBoolean)
+
